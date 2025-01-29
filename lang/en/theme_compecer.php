875aab2177dd71de79c6474356cf3ce18ce829fc
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2025-01-29 09:18:41 -0500

    Update theme_compecer.php

--- lang/en/theme_compecer.php
+++ lang/en/theme_compecer.php
@@ -89,3 +89,66 @@ $string['hide'] = 'Hide';
 
 // Cache strings
 $string['cachedef_fontawesomemooveiconmapping'] = 'Theme font icon mapping';
+
+// Tab names
+$string['generalsettings'] = 'General Settings';
+$string['slidersettings'] = 'Slider Settings';
+$string['aboutsettings'] = 'About Settings';
+$string['careersettings'] = 'Career Settings';
+$string['chatsettings'] = 'Chat Settings';
+$string['accessibilitysettings'] = 'Accessibility Settings';
+$string['copypastesettings'] = 'Copy/Paste Settings';
+$string['loginsettings'] = 'Login Settings';
+
+// Slider Strings
+$string['slidercount'] = 'Number of Slides';
+$string['slidercountdesc'] = 'Select how many slides you want to add';
+$string['sliderimage'] = 'Slide {$a} Image';
+$string['sliderimagedesc'] = 'Add an image for this slide';
+$string['slidertitle'] = 'Slide {$a} Title';
+$string['slidertitledesc'] = 'Add a title for this slide';
+$string['slidercaption'] = 'Slide {$a} Caption';
+$string['slidercaptiondesc'] = 'Add a caption for this slide';
+
+// About Section Strings
+$string['abouttitle'] = 'About Title';
+$string['abouttitledesc'] = 'The title for your About section';
+$string['aboutcontent'] = 'About Content';
+$string['aboutcontentdesc'] = 'The main content of your About section';
+$string['abouttitledefault'] = 'Who We Are';
+$string['aboutcontentdefault'] = 'Add your organization description here';
+
+// Career Section Strings
+$string['careersection_title'] = 'Invierte en tu carrera';
+$string['enablecareerboxes'] = 'Enable Career Boxes';
+$string['enablecareerboxesdesc'] = 'Enable or disable the career boxes section';
+$string['careerboxicon'] = 'Box {$a} Icon';
+$string['careerboxicondesc'] = 'Select an icon for this career box';
+$string['careerboxtitle'] = 'Box {$a} Title';
+$string['careerboxtitledesc'] = 'Enter the title for this career box';
+$string['careerboxcontent'] = 'Box {$a} Content';
+$string['careerboxcontentdesc'] = 'Enter the content for this career box';
+$string['careerboxtitledefault'] = 'Career Path {$a}';
+$string['careerboxcontentdefault'] = 'Describe career path {$a} here';
+
+// Icon Strings
+$string['icon_graduation'] = 'Graduation Cap';
+$string['icon_book'] = 'Book';
+$string['icon_laptop'] = 'Laptop';
+$string['icon_users'] = 'Users';
+$string['icon_globe'] = 'Globe';
+$string['icon_lightbulb'] = 'Light Bulb';
+$string['icon_chart'] = 'Chart Line';
+$string['icon_medal'] = 'Medal';
+$string['icon_certificate'] = 'Certificate';
+$string['icon_star'] = 'Star';
+$string['icon_rocket'] = 'Rocket';
+$string['icon_code'] = 'Code';
+$string['icon_microscope'] = 'Microscope';
+$string['icon_flask'] = 'Flask';
+$string['icon_atom'] = 'Atom';
+$string['icon_brain'] = 'Brain';
+$string['icon_university'] = 'University';
+$string['icon_award'] = 'Award';
+$string['icon_usergraduate'] = 'Graduate';
+$string['icon_teacher'] = 'Teacher';
