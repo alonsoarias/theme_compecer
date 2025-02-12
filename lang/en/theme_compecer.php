e51a047ce5515feb1af26be371b358974dbde35e
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2025-02-12 08:35:19 -0500

    Update theme_compecer.php

--- lang/en/theme_compecer.php
+++ lang/en/theme_compecer.php
@@ -24,186 +24,191 @@
  */
 
 // General
-$string['pluginname'] = 'Compecer';
-$string['choosereadme'] = 'Compecer is a theme created by IngeWeb exclusively for our customers.';
-$string['configtitle'] = 'Compecer Settings';
-$string['themesettings'] = 'Theme Settings';
-$string['region-side-pre'] = 'Right';
+$string['pluginname']              = 'Compecer';
+$string['choosereadme']             = 'Compecer is a theme created by IngeWeb exclusively for our customers.';
+$string['configtitle']             = 'Compecer Settings';
+$string['themesettings']           = 'Theme Settings';
+$string['region-side-pre']         = 'Right';
 
 // Section headers
-$string['themesettingsgeneral'] = 'General Settings';
+$string['themesettingsgeneral']    = 'General Settings';
 $string['themesettingsgeneraldesc'] = 'General theme configuration options';
-$string['themesettingschat'] = 'Chat Settings';
-$string['themesettingschatdesc'] = 'Configure the chat integration options';
-$string['themesettingsaccessibility'] = 'Accessibility Settings';
-$string['themesettingsaccessibilitydesc'] = 'Configure accessibility options';
-$string['themesettingscopypaste'] = 'Copy/Paste Settings';
+$string['themesettingschat']       = 'Chat Settings';
+$string['themesettingschatdesc']   = 'Configure the chat integration options';
+$string['themesettingsaccessibility']       = 'Accessibility Settings';
+$string['themesettingsaccessibilitydesc']   = 'Configure accessibility options';
+$string['themesettingscopypaste']  = 'Copy/Paste Settings';
 $string['themesettingscopypaste_desc'] = 'Configure copy and paste restrictions';
-$string['themesettingslogin'] = 'Login Settings';
-$string['themesettingslogindesc'] = 'Configure login page appearance';
-$string['themesettingsabout'] = 'About Settings';
-$string['themesettingsaboutdesc'] = 'Configure about section settings';
+$string['themesettingslogin']      = 'Login Settings';
+$string['themesettingslogindesc']  = 'Configure login page appearance';
+$string['themesettingsabout']      = 'About Settings';
+$string['themesettingsaboutdesc']  = 'Configure about section settings';
 
 // General settings
-$string['themeinfotext'] = 'This theme was created for <strong> another Moodle project </strong> by <a target="_blank" href="http://ingeweb.co/">IngeWeb - Soluciones para triunfar en Internet</a>.';
-$string['generalnoticemode'] = 'Notice Mode';
-$string['generalnoticemodedesc'] = 'The general notice was created to allow administrators to show important notices in the header in every page after logging in.';
-$string['generalnoticemode_off'] = 'Off';
-$string['generalnoticemode_info'] = 'Info';
-$string['generalnoticemode_danger'] = 'Critical';
-$string['generalnotice'] = 'Notice text';
-$string['generalnoticedesc'] = 'Text to show inside the alert box';
-$string['generalnotice_create'] = 'Set a notice';
-$string['generalnoticedefault'] = '<strong>We are working</strong> to improve. The platform may experience strange behaviors at times.';
+$string['themeinfotext']           = 'This theme was created for <strong> another Moodle project </strong> by <a target="_blank" href="http://ingeweb.co/">IngeWeb - Soluciones para triunfar en Internet</a>.';
+$string['generalnoticemode']       = 'Notice Mode';
+$string['generalnoticemodedesc']   = 'The general notice was created to allow administrators to show important notices in the header in every page after logging in.';
+$string['generalnoticemode_off']   = 'Off';
+$string['generalnoticemode_info']  = 'Info';
+$string['generalnoticemode_danger']= 'Critical';
+$string['generalnotice']           = 'Notice text';
+$string['generalnoticedesc']       = 'Text to show inside the alert box';
+$string['generalnotice_create']    = 'Set a notice';
+$string['generalnoticedefault']    = '<strong>We are working</strong> to improve. The platform may experience strange behaviors at times.';
 
 // Section visibility toggles
-$string['enable_slider'] = 'Enable slider section';
-$string['enable_sliderdesc'] = 'Enable or disable the slider section on the frontpage';
-$string['enable_about'] = 'Enable about section';
-$string['enable_aboutdesc'] = 'Enable or disable the about section on the frontpage';
-$string['enable_search_categories'] = 'Enable search and categories section';
+$string['enable_slider']           = 'Enable slider section';
+$string['enable_sliderdesc']       = 'Enable or disable the slider section on the frontpage';
+$string['enable_about']            = 'Enable about section';
+$string['enable_aboutdesc']        = 'Enable or disable the about section on the frontpage';
+$string['enable_search_categories']= 'Enable search and categories section';
 $string['enable_search_categoriesdesc'] = 'Enable or disable the search and categories section on the frontpage';
 
 // Dashboard Cards Settings
-$string['mydashboardsettings'] = 'Dashboard Cards Settings';
-$string['enable_dashboard_cards'] = 'Enable Dashboard Cards';
+$string['mydashboardsettings']     = 'Dashboard Cards Settings';
+$string['enable_dashboard_cards']  = 'Enable Dashboard Cards';
 $string['enable_dashboard_cardsdesc'] = 'Enable or disable the dashboard cards section';
-$string['dashboard_card_heading'] = 'Dashboard Card {$a}';
+$string['dashboard_card_heading']  = 'Dashboard Card {$a}';
 $string['dashboard_card_visibility'] = 'Show Card {$a}';
 $string['dashboard_card_visibilitydesc'] = 'Enable or disable dashboard card {$a}';
-$string['dashboard_card_title'] = 'Card {$a} Title';
+$string['dashboard_card_title']    = 'Card {$a} Title';
 $string['dashboard_card_titledesc'] = 'Enter the title for dashboard card {$a}';
 $string['dashboard_card_subtitle'] = 'Card {$a} Subtitle';
 $string['dashboard_card_subtitledesc'] = 'Enter the subtitle for dashboard card {$a}';
-$string['dashboard_card_url'] = 'Card {$a} URL';
-$string['dashboard_card_urldesc'] = 'Enter the URL for dashboard card {$a}';
-$string['dashboard_card_color'] = 'Card {$a} Color';
-$string['dashboard_card_colordesc'] = 'Choose the color for dashboard card {$a}';
-$string['dashboard_card_icon'] = 'Card {$a} Icon';
+$string['dashboard_card_url']      = 'Card {$a} URL';
+$string['dashboard_card_urldesc']  = 'Enter the URL for dashboard card {$a}';
+$string['dashboard_card_color']    = 'Card {$a} Color';
+$string['dashboard_card_colordesc']= 'Choose the color for dashboard card {$a}';
+$string['dashboard_card_icon']     = 'Card {$a} Icon';
 $string['dashboard_card_icondesc'] = 'Choose the icon for dashboard card {$a}';
 
 // Chat settings
-$string['enable_chat'] = 'Enable chat widget';
-$string['enable_chatdesc'] = 'Enable Tawk.to chat widget integration';
-$string['tawkto_embed_url'] = 'Tawk.to URL';
-$string['tawkto_embed_urldesc'] = 'Enter your Tawk.to property URL (e.g., https://embed.tawk.to/YOUR_ID/default)';
+$string['enable_chat']             = 'Enable chat widget';
+$string['enable_chatdesc']         = 'Enable Tawk.to chat widget integration';
+$string['tawkto_embed_url']        = 'Tawk.to URL';
+$string['tawkto_embed_urldesc']    = 'Enter your Tawk.to property URL (e.g., https://embed.tawk.to/YOUR_ID/default)';
+$string['tawkto_embed_urldefault'] = 'https://embed.tawk.to/YOUR_DEFAULT_ID/default';
 
 // Accessibility settings
-$string['accessibility_widget'] = 'Enable Accessibility Widget';
-$string['accessibility_widgetdesc'] = 'Enable or disable the accessibility support widget';
+$string['accessibility_widget']    = 'Enable Accessibility Widget';
+$string['accessibility_widgetdesc']= 'Enable or disable the accessibility support widget';
 
 // Copy/Paste settings
-$string['copypaste_prevention'] = 'Enable Copy/Paste Prevention';
-$string['copypaste_preventiondesc'] = 'Prevent copying and pasting content for selected roles';
-$string['copypaste_roles'] = 'Restricted Roles';
-$string['copypaste_rolesdesc'] = 'Select which roles should have copy/paste restrictions';
+$string['copypaste_prevention']    = 'Enable Copy/Paste Prevention';
+$string['copypaste_preventiondesc']= 'Prevent copying and pasting content for selected roles';
+$string['copypaste_roles']         = 'Restricted Roles';
+$string['copypaste_rolesdesc']     = 'Select which roles should have copy/paste restrictions';
 
 // Login page
-$string['loginbg_image'] = 'Login Background Image';
-$string['loginbg_imagedesc'] = 'Upload a background image for the login page';
-$string['loginbg_color'] = 'Login Background Color';
-$string['loginbg_colordesc'] = 'Background color in login page';
-$string['credit'] = 'Theme created by <a target="_blank" class="my-credit-link" href="http://ingeweb.co/">www.ingeweb.co</a>';
+$string['loginbg_image']           = 'Login Background Image';
+$string['loginbg_imagedesc']       = 'Upload a background image for the login page';
+$string['loginbg_color']           = 'Login Background Color';
+$string['loginbg_colordesc']       = 'Background color in login page';
+$string['credit']                  = 'Theme created by <a target="_blank" class="my-credit-link" href="http://ingeweb.co/">www.ingeweb.co</a>';
 
 // Tab names
-$string['generalsettings'] = 'General Settings';
-$string['slidersettings'] = 'Slider Settings';
-$string['aboutsettings'] = 'About Settings';
-$string['careersettings'] = 'Career Settings';
-$string['chatsettings'] = 'Chat Settings';
-$string['accessibilitysettings'] = 'Accessibility Settings';
-$string['copypastesettings'] = 'Copy/Paste Settings';
-$string['loginsettings'] = 'Login Settings';
+$string['generalsettings']         = 'General Settings';
+$string['slidersettings']          = 'Slider Settings';
+$string['aboutsettings']           = 'About Settings';
+$string['careersettings']          = 'Career Settings';
+$string['chatsettings']            = 'Chat Settings';
+$string['accessibilitysettings']   = 'Accessibility Settings';
+$string['copypastesettings']       = 'Copy/Paste Settings';
+$string['loginsettings']           = 'Login Settings';
 
 // Slider Strings
-$string['slidercount'] = 'Number of Slides';
-$string['slidercountdesc'] = 'Select how many slides you want to add';
-$string['sliderimage'] = 'Slide {$a} Image';
-$string['sliderimagedesc'] = 'Add an image for this slide';
-$string['slidertitle'] = 'Slide {$a} Title';
-$string['slidertitledesc'] = 'Add a title for this slide';
-$string['slidercaption'] = 'Slide {$a} Caption';
-$string['slidercaptiondesc'] = 'Add a caption for this slide';
+$string['slidercount']             = 'Number of Slides';
+$string['slidercountdesc']         = 'Select how many slides you want to add';
+$string['sliderimage']             = 'Slide {$a} Image';
+$string['sliderimagedesc']         = 'Add an image for this slide';
+$string['slidertitle']             = 'Slide {$a} Title';
+$string['slidertitledesc']         = 'Add a title for this slide';
+$string['slidercaption']           = 'Slide {$a} Caption';
+$string['slidercaptiondesc']       = 'Add a caption for this slide';
+// New strings for default slider values.
+$string['slidertitledefault']      = 'Slide {$a} Default Title';
+$string['slidercaptiondefault']    = 'Slide {$a} Default Caption';
 
 // About Section Strings
-$string['abouttitle'] = 'About Title';
-$string['abouttitledesc'] = 'The title for your About section';
-$string['aboutcontent'] = 'About Content';
-$string['aboutcontentdesc'] = 'The main content of your About section';
-$string['abouttitledefault'] = 'Who We Are';
-$string['aboutcontentdefault'] = 'Add your organization description here';
+$string['abouttitle']              = 'About Title';
+$string['abouttitledesc']          = 'The title for your About section';
+$string['aboutcontent']            = 'About Content';
+$string['aboutcontentdesc']        = 'The main content of your About section';
+$string['abouttitledefault']       = 'Who We Are';
+$string['aboutcontentdefault']     = 'Add your organization description here';
 
 // Career Section Strings
-$string['careersection_title'] = 'Invierte en tu carrera';
-$string['enablecareerboxes'] = 'Enable Career Boxes';
-$string['enablecareerboxesdesc'] = 'Enable or disable the career boxes section';
-$string['careerboxesbgcolor'] = 'Career section background color';
-$string['careerboxesbgcolordesc'] = 'Set the background color for the career section';
-$string['careerboxicon'] = 'Box {$a} Icon';
-$string['careerboxicondesc'] = 'Select an icon for this career box';
-$string['careerboxtitle'] = 'Box {$a} Title';
-$string['careerboxtitledesc'] = 'Enter the title for this career box';
-$string['careerboxcontent'] = 'Box {$a} Content';
-$string['careerboxcontentdesc'] = 'Enter the content for this career box';
-$string['careerboxtitledefault'] = 'Career Path {$a}';
+$string['careersection_title']     = 'Invest in Your Career';
+$string['enablecareerboxes']       = 'Enable Career Boxes';
+$string['enablecareerboxesdesc']   = 'Enable or disable the career boxes section';
+$string['careerboxesbgcolor']      = 'Career section background color';
+$string['careerboxesbgcolordesc']  = 'Set the background color for the career section';
+$string['careerboxicon']           = 'Box {$a} Icon';
+$string['careerboxicondesc']       = 'Select an icon for this career box';
+$string['careerboxtitle']          = 'Box {$a} Title';
+$string['careerboxtitledesc']      = 'Enter the title for this career box';
+$string['careerboxcontent']        = 'Box {$a} Content';
+$string['careerboxcontentdesc']    = 'Enter the content for this career box';
+$string['careerboxtitledefault']   = 'Career Path {$a}';
 $string['careerboxcontentdefault'] = 'Describe career path {$a} here';
 
 // Icon Strings
-$string['icon_message'] = 'Message';
-$string['icon_user'] = 'User';
-$string['icon_settings'] = 'Settings';
-$string['icon_chart_line'] = 'Line Chart';
-$string['icon_chart_bar'] = 'Bar Chart';
-$string['icon_graduation'] = 'Graduation Cap';
-$string['icon_book'] = 'Book';
-$string['icon_laptop'] = 'Laptop';
-$string['icon_users'] = 'Users';
-$string['icon_globe'] = 'Globe';
-$string['icon_lightbulb'] = 'Light Bulb';
-$string['icon_chart'] = 'Chart Line';
-$string['icon_medal'] = 'Medal';
-$string['icon_certificate'] = 'Certificate';
-$string['icon_star'] = 'Star';
-$string['icon_rocket'] = 'Rocket';
-$string['icon_code'] = 'Code';
-$string['icon_microscope'] = 'Microscope';
-$string['icon_flask'] = 'Flask';
-$string['icon_atom'] = 'Atom';
-$string['icon_brain'] = 'Brain';
-$string['icon_university'] = 'University';
-$string['icon_award'] = 'Award';
-$string['icon_usergraduate'] = 'Graduate';
-$string['icon_teacher'] = 'Teacher';
+$string['icon_message']            = 'Message';
+$string['icon_user']               = 'User';
+$string['icon_settings']           = 'Settings';
+$string['icon_chart_line']         = 'Line Chart';
+$string['icon_chart_bar']          = 'Bar Chart';
+$string['icon_graduation']         = 'Graduation Cap';
+$string['icon_book']               = 'Book';
+$string['icon_laptop']             = 'Laptop';
+$string['icon_users']              = 'Users';
+$string['icon_globe']              = 'Globe';
+$string['icon_lightbulb']          = 'Light Bulb';
+$string['icon_chart']              = 'Chart Line';
+$string['icon_medal']              = 'Medal';
+$string['icon_certificate']        = 'Certificate';
+$string['icon_star']               = 'Star';
+$string['icon_rocket']             = 'Rocket';
+$string['icon_code']               = 'Code';
+$string['icon_microscope']         = 'Microscope';
+$string['icon_flask']              = 'Flask';
+$string['icon_atom']               = 'Atom';
+$string['icon_brain']              = 'Brain';
+$string['icon_university']         = 'University';
+$string['icon_award']              = 'Award';
+$string['icon_usergraduate']       = 'Graduate';
+$string['icon_teacher']            = 'Teacher';
 
 // Search and Categories Strings
-$string['categoriessettings'] = 'Categories Settings';
-$string['selectedcategories'] = 'Selected Categories';
-$string['selectedcategoriesdesc'] = 'Choose which categories to display in the frontpage';
-$string['searchcourses'] = 'Search courses';
-$string['searchsectiontitle'] = 'Search Section Title';
-$string['searchsectiontitledesc'] = 'Title for the search section';
+$string['categoriessettings']      = 'Categories Settings';
+$string['selectedcategories']      = 'Selected Categories';
+$string['selectedcategoriesdesc']  = 'Choose which categories to display in the frontpage';
+$string['searchcourses']           = 'Search courses';
+$string['searchsectiontitle']      = 'Search Section Title';
+$string['searchsectiontitledesc']  = 'Title for the search section';
 $string['searchsectiontitledefault'] = 'Find Your Course';
-$string['searchsectiondesc'] = 'Search Section Description';
-$string['searchsectiondescdesc'] = 'Description text for the search section';
+$string['searchsectiondesc']       = 'Search Section Description';
+$string['searchsectiondescdesc']   = 'Description text for the search section';
 $string['searchsectiondescdefault'] = 'Search through our wide range of courses';
-$string['categoriesbgcolor'] = 'Categories Background Color';
-$string['categoriesbgcolordesc'] = 'Background color for the categories section';
+$string['categoriesbgcolor']       = 'Categories Background Color';
+$string['categoriesbgcolordesc']   = 'Background color for the categories section';
 
 // Visibility strings
-$string['show'] = 'Show';
-$string['hide'] = 'Hide';
-$string['config_visibility'] = 'Visibility';
-$string['config_visibility_desc'] = 'Show or hide this element';
-$string['config_title'] = 'Title';
-$string['config_title_desc'] = 'Enter the title for this element';
-$string['config_subtitle'] = 'Subtitle';
-$string['config_subtitle_desc'] = 'Enter the subtitle for this element';
-$string['config_link'] = 'Link URL';
-$string['config_link_desc'] = 'Enter the URL this element should link to';
-$string['config_color'] = 'Color';
-$string['config_color_desc'] = 'Choose the color for this element';
-$string['config_icon_class'] = 'Icon';
-$string['config_icon_class_desc'] = 'Choose the icon for this element';
+$string['show']                    = 'Show';
+$string['hide']                    = 'Hide';
+$string['config_visibility']       = 'Visibility';
+$string['config_visibility_desc']  = 'Show or hide this element';
+$string['config_title']            = 'Title';
+$string['config_title_desc']       = 'Enter the title for this element';
+$string['config_subtitle']         = 'Subtitle';
+$string['config_subtitle_desc']    = 'Enter the subtitle for this element';
+$string['config_link']             = 'Link URL';
+$string['config_link_desc']        = 'Enter the URL this element should link to';
+$string['config_color']            = 'Color';
+$string['config_color_desc']       = 'Choose the color for this element';
+$string['config_icon_class']       = 'Icon';
+$string['config_icon_class_desc']  = 'Choose the icon for this element';
 
 // Cache strings
 $string['cachedef_fontawesomemooveiconmapping'] = 'Theme font icon mapping';
+$string['breadcrumb']='Breadcrumb';
