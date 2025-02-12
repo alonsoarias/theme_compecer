8d8001eda7fb7fc5dcac410c1e8f57d22637ab84
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2025-02-12 13:02:54 -0500

    Create theme_compecer.php

---
+++ lang/es/theme_compecer.php
@@ -0,0 +1,213 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file.
+ *
+ * @package    theme_compecer
+ * @copyright  2024 IngeWeb https://www.ingeweb.co
+ * @author     Pedro Arias <soporte@ingeweb.co>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// General
+$string['pluginname']              = 'Compecer';
+$string['choosereadme']           = 'Compecer es un tema creado por IngeWeb exclusivamente para nuestros clientes.';
+$string['configtitle']            = 'Configuración de Compecer';
+$string['themesettings']          = 'Configuración del Tema';
+$string['region-side-pre']        = 'Derecha';
+
+// Section headers
+$string['themesettingsgeneral']    = 'Configuración General';
+$string['themesettingsgeneraldesc'] = 'Opciones generales de configuración del tema';
+$string['themesettingschat']       = 'Configuración del Chat';
+$string['themesettingschatdesc']   = 'Configurar las opciones de integración del chat';
+$string['themesettingsaccessibility'] = 'Configuración de Accesibilidad';
+$string['themesettingsaccessibilitydesc'] = 'Configurar opciones de accesibilidad';
+$string['themesettingscopypaste']  = 'Configuración de Copiar/Pegar';
+$string['themesettingscopypaste_desc'] = 'Configurar restricciones de copiar y pegar';
+$string['themesettingslogin']      = 'Configuración de Inicio de Sesión';
+$string['themesettingslogindesc']  = 'Configurar apariencia de la página de inicio de sesión';
+$string['themesettingsabout']      = 'Configuración de Acerca de';
+$string['themesettingsaboutdesc']  = 'Configurar ajustes de la sección Acerca de';
+
+// General settings
+$string['themeinfotext']           = 'Este tema fue creado para <strong>otro proyecto Moodle</strong> por <a target="_blank" href="http://ingeweb.co/">IngeWeb - Soluciones para triunfar en Internet</a>.';
+$string['generalnoticemode']       = 'Modo de Aviso';
+$string['generalnoticemodedesc']   = 'El aviso general fue creado para permitir a los administradores mostrar avisos importantes en el encabezado en cada página después de iniciar sesión.';
+$string['generalnoticemode_off']   = 'Apagado';
+$string['generalnoticemode_info']  = 'Información';
+$string['generalnoticemode_danger']= 'Crítico';
+$string['generalnotice']           = 'Texto del aviso';
+$string['generalnoticedesc']       = 'Texto para mostrar dentro del cuadro de alerta';
+$string['generalnotice_create']    = 'Establecer un aviso';
+$string['generalnoticedefault']    = '<strong>Estamos trabajando</strong> para mejorar. La plataforma puede experimentar comportamientos extraños en ocasiones.';
+
+// Section visibility toggles
+$string['enable_slider']           = 'Habilitar sección de carrusel';
+$string['enable_sliderdesc']       = 'Habilitar o deshabilitar la sección de carrusel en la página principal';
+$string['enable_about']            = 'Habilitar sección Acerca de';
+$string['enable_aboutdesc']        = 'Habilitar o deshabilitar la sección Acerca de en la página principal';
+$string['enable_search_categories']= 'Habilitar sección de búsqueda y categorías';
+$string['enable_search_categoriesdesc'] = 'Habilitar o deshabilitar la sección de búsqueda y categorías en la página principal';
+
+// Dashboard Cards Settings
+$string['mydashboardsettings']     = 'Configuración de Tarjetas del Panel';
+$string['enable_dashboard_cards']  = 'Habilitar Tarjetas del Panel';
+$string['enable_dashboard_cardsdesc'] = 'Habilitar o deshabilitar la sección de tarjetas del panel';
+$string['dashboard_card_heading']  = 'Tarjeta del Panel {$a}';
+$string['dashboard_card_visibility'] = 'Mostrar Tarjeta {$a}';
+$string['dashboard_card_visibilitydesc'] = 'Habilitar o deshabilitar tarjeta del panel {$a}';
+$string['dashboard_card_title']    = 'Título de Tarjeta {$a}';
+$string['dashboard_card_titledesc'] = 'Ingrese el título para la tarjeta del panel {$a}';
+$string['dashboard_card_subtitle'] = 'Subtítulo de Tarjeta {$a}';
+$string['dashboard_card_subtitledesc'] = 'Ingrese el subtítulo para la tarjeta del panel {$a}';
+$string['dashboard_card_url']      = 'URL de Tarjeta {$a}';
+$string['dashboard_card_urldesc']  = 'Ingrese la URL para la tarjeta del panel {$a}';
+$string['dashboard_card_color']    = 'Color de Tarjeta {$a}';
+$string['dashboard_card_colordesc']= 'Elija el color para la tarjeta del panel {$a}';
+$string['dashboard_card_icon']     = 'Icono de Tarjeta {$a}';
+$string['dashboard_card_icondesc'] = 'Elija el icono para la tarjeta del panel {$a}';
+
+// Chat settings
+$string['enable_chat']             = 'Habilitar widget de chat';
+$string['enable_chatdesc']         = 'Habilitar integración del widget de Tawk.to';
+$string['tawkto_embed_url']        = 'URL de Tawk.to';
+$string['tawkto_embed_urldesc']    = 'Ingrese su URL de propiedad de Tawk.to (ej., https://embed.tawk.to/TU_ID/default)';
+$string['tawkto_embed_urldefault'] = 'https://embed.tawk.to/TU_ID_PREDETERMINADO/default';
+
+// Accessibility settings
+$string['accessibility_widget']    = 'Habilitar Widget de Accesibilidad';
+$string['accessibility_widgetdesc']= 'Habilitar o deshabilitar el widget de soporte de accesibilidad';
+
+// Copy/Paste settings
+$string['copypaste_prevention']    = 'Habilitar Prevención de Copiar/Pegar';
+$string['copypaste_preventiondesc']= 'Prevenir copiar y pegar contenido para roles seleccionados';
+$string['copypaste_roles']         = 'Roles Restringidos';
+$string['copypaste_rolesdesc']     = 'Seleccionar qué roles deberían tener restricciones de copiar/pegar';
+
+// Login page
+$string['loginbg_image']           = 'Imagen de Fondo de Inicio de Sesión';
+$string['loginbg_imagedesc']       = 'Subir una imagen de fondo para la página de inicio de sesión';
+$string['loginbg_color']           = 'Color de Fondo de Inicio de Sesión';
+$string['loginbg_colordesc']       = 'Color de fondo en la página de inicio de sesión';
+$string['credit']                  = 'Tema creado por <a target="_blank" class="my-credit-link" href="http://ingeweb.co/">www.ingeweb.co</a>';
+
+// Tab names
+$string['generalsettings']         = 'Configuración General';
+$string['slidersettings']          = 'Configuración del Carrusel';
+$string['aboutsettings']           = 'Configuración de Acerca de';
+$string['careersettings']          = 'Configuración de Carrera';
+$string['chatsettings']            = 'Configuración del Chat';
+$string['accessibilitysettings']   = 'Configuración de Accesibilidad';
+$string['copypastesettings']       = 'Configuración de Copiar/Pegar';
+$string['loginsettings']           = 'Configuración de Inicio de Sesión';
+
+// Slider Strings
+$string['slidercount']             = 'Número de Diapositivas';
+$string['slidercountdesc']         = 'Seleccione cuántas diapositivas desea agregar';
+$string['sliderimage']             = 'Imagen de Diapositiva {$a}';
+$string['sliderimagedesc']         = 'Agregar una imagen para esta diapositiva';
+$string['slidertitle']             = 'Título de Diapositiva {$a}';
+$string['slidertitledesc']         = 'Agregar un título para esta diapositiva';
+$string['slidercaption']           = 'Leyenda de Diapositiva {$a}';
+$string['slidercaptiondesc']       = 'Agregar una leyenda para esta diapositiva';
+$string['slidertitledefault']      = 'Título Predeterminado de Diapositiva {$a}';
+$string['slidercaptiondefault']    = 'Leyenda Predeterminada de Diapositiva {$a}';
+
+// About Section Strings
+$string['abouttitle']              = 'Título de Acerca de';
+$string['abouttitledesc']          = 'El título para su sección Acerca de';
+$string['aboutcontent']            = 'Contenido de Acerca de';
+$string['aboutcontentdesc']        = 'El contenido principal de su sección Acerca de';
+$string['abouttitledefault']       = 'Quiénes Somos';
+$string['aboutcontentdefault']     = 'Agregue aquí la descripción de su organización';
+
+// Career Section Strings
+$string['careersection_title']     = 'Invierte en Tu Carrera';
+$string['enablecareerboxes']       = 'Habilitar Cajas de Carrera';
+$string['enablecareerboxesdesc']   = 'Habilitar o deshabilitar la sección de cajas de carrera';
+$string['careerboxesbgcolor']      = 'Color de fondo de la sección de carrera';
+$string['careerboxesbgcolordesc']  = 'Establecer el color de fondo para la sección de carrera';
+$string['careerboxicon']           = 'Icono de Caja {$a}';
+$string['careerboxicondesc']       = 'Seleccionar un icono para esta caja de carrera';
+$string['careerboxtitle']          = 'Título de Caja {$a}';
+$string['careerboxtitledesc']      = 'Ingrese el título para esta caja de carrera';
+$string['careerboxcontent']        = 'Contenido de Caja {$a}';
+$string['careerboxcontentdesc']    = 'Ingrese el contenido para esta caja de carrera';
+$string['careerboxtitledefault']   = 'Trayectoria Profesional {$a}';
+$string['careerboxcontentdefault'] = 'Describa la trayectoria profesional {$a} aquí';
+
+// Icon Strings
+$string['icon_message']            = 'Mensaje';
+$string['icon_user']               = 'Usuario';
+$string['icon_settings']           = 'Configuración';
+$string['icon_chart_line']         = 'Gráfico de Línea';
+$string['icon_chart_bar']          = 'Gráfico de Barras';
+$string['icon_graduation']         = 'Birrete de Graduación';
+$string['icon_book']               = 'Libro';
+$string['icon_laptop']             = 'Portátil';
+$string['icon_users']              = 'Usuarios';
+$string['icon_globe']              = 'Globo';
+$string['icon_lightbulb']          = 'Bombilla';
+$string['icon_chart']              = 'Línea de Gráfico';
+$string['icon_medal']              = 'Medalla';
+$string['icon_certificate']        = 'Certificado';
+$string['icon_star']               = 'Estrella';
+$string['icon_rocket']             = 'Cohete';
+$string['icon_code']               = 'Código';
+$string['icon_microscope']         = 'Microscopio';
+$string['icon_flask']              = 'Matraz';
+$string['icon_atom']               = 'Átomo';
+$string['icon_brain']              = 'Cerebro';
+$string['icon_university']         = 'Universidad';
+$string['icon_award']              = 'Premio';
+$string['icon_usergraduate']       = 'Graduado';
+$string['icon_teacher']            = 'Profesor';
+
+// Search and Categories Strings
+$string['categoriessettings']      = 'Configuración de Categorías';
+$string['selectedcategories']      = 'Categorías Seleccionadas';
+$string['selectedcategoriesdesc']  = 'Elija qué categorías mostrar en la página principal';
+$string['searchcourses']           = 'Buscar cursos';
+$string['searchsectiontitle']      = 'Título de Sección de Búsqueda';
+$string['searchsectiontitledesc']  = 'Título para la sección de búsqueda';
+$string['searchsectiontitledefault'] = 'Encuentra Tu Curso';
+$string['searchsectiondesc']       = 'Descripción de Sección de Búsqueda';
+$string['searchsectiondescdesc']   = 'Texto de descripción para la sección de búsqueda';
+$string['searchsectiondescdefault'] = 'Busca entre nuestra amplia gama de cursos';
+$string['categoriesbgcolor']       = 'Color de Fondo de Categorías';
+$string['categoriesbgcolordesc']   = 'Color de fondo para la sección de categorías';
+
+// Visibility strings
+$string['show']                    = 'Mostrar';
+$string['hide']                    = 'Ocultar';
+$string['config_visibility']       = 'Visibilidad';
+$string['config_visibility_desc']  = 'Mostrar u ocultar este elemento';
+$string['config_title']            = 'Título';
+$string['config_title_desc']       = 'Ingrese el título para este elemento';
+$string['config_subtitle']         = 'Subtítulo';
+$string['config_subtitle_desc']    = 'Ingrese el subtítulo para este elemento';
+$string['config_link']             = 'URL del Enlace';
+$string['config_link_desc']        = 'Ingrese la URL a la que debe enlazar este elemento';
+$string['config_color']            = 'Color';
+$string['config_color_desc']       = 'Elija el color para este elemento';
+$string['config_icon_class']       = 'Icono';
+$string['config_icon_class_desc']  = 'Elija el icono para este elemento';
+
+// Cache strings
+$string['cachedef_fontawesomemooveiconmapping'] = 'Mapeo de iconos de fuente del tema';
+$string['breadcrumb']             = 'Ruta de navegación';
